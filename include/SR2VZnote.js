920a0924c96657275575e5737462eea86ae0e29d
YorktownLaogou <tridentLaogou@gmail.com>
2026-07-25 22:51:51 +0900

    capitalization wrong
    I got the capitalization wrong, not sure why it doesn't throw an error in my environment.

--- include/SR2VZnote.js
+++ include/SR2VZnote.js
@@ -1,6 +1,6 @@
 import "./VizzyForWeb/SR2VIZZY.js"
 import "./LfCatalogue.js"
-import "./lfContent.js";
+import "./LfContent.js";
 
 import { Catalogue } from "./catalogue-data.js";
 const menu = document.querySelector("lf-catalogue");
